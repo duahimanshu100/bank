a9474d081ada01e71213af7e0ea36160a6f7b9e3
Himanshu dua <himanshu.dua@decorist.com>
2019-09-21 16:27:51 +0530

    Added Delete view and permission for add update

--- bank/debtor/urls.py
+++ bank/debtor/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from bank.debtor.views import DebtorCreate, DebtorList, DebtorUpdateView
+from bank.debtor.views import DebtorCreate, DebtorList, DebtorUpdateView, DebtorDeleteView
 
 urlpatterns = [
     path('', DebtorList.as_view(), name='debtor-list'),
     path('create', DebtorCreate.as_view(), name='debtor-create'),
     path('<int:pk>/change', DebtorUpdateView.as_view(), name='debtor-update'),
+    path('<int:pk>/delete', DebtorDeleteView.as_view(), name='debtor-delete'),
 
 ]
--- bank/debtor/views.py
+++ bank/debtor/views.py
@@ -2,7 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 
 # Create your views here.
-from django.views.generic import ListView, UpdateView, CreateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
 
 from bank.bank_account.models import Account
 from bank.debtor.models import Debtor
@@ -12,26 +13,36 @@ class DebtorList(LoginRequiredMixin, ListView):
     model = Debtor
 
     def get_queryset(self):
-        return Debtor.objects.filter(owner=self.request.user)
+        owner = self.request.user
+        return self.model.objects.filter(owner=owner)
 
 
 class DebtorUpdateView(LoginRequiredMixin, UpdateView):
     model = Debtor
     fields = ['first_name', 'last_name', 'email', ]
+    success_url = reverse_lazy('debtor-list')
 
-    def get_success_url(self):
-        return '/debtors'
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(owner=owner)
 
 
 class DebtorCreate(LoginRequiredMixin, CreateView):
     model = Debtor
     fields = ['first_name', 'last_name', 'email', ]
 
-    def get_success_url(self):
-        return '/debtors'
+    success_url = reverse_lazy('debtor-list')
 
     def form_valid(self, form):
         form.instance.owner = self.request.user
         form.instance.account = Account.objects.create()
-        # account =
         return super(DebtorCreate, self).form_valid(form)
+
+    def get_queryset(self):
+        owner = self.request.user
+        return self.model.objects.filter(owner=owner)
+
+
+class DebtorDeleteView(DeleteView):
+    model = Debtor
+    success_url = reverse_lazy('debtor-list')
