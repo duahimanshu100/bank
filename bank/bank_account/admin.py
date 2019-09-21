bb64c093345de705e9694e4ee4041d7932c1c05c
Himanshu dua <himanshu.dua@decorist.com>
2019-09-21 16:30:09 +0530

    Admin changes

--- bank/bank_account/admin.py
+++ bank/bank_account/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
+from bank.bank_account.models import Account
+
+admin.site.register(Account)
--- bank/debtor/admin.py
+++ bank/debtor/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from bank.debtor.models import Debtor
+from bank.invoice.models import Invoice
+
+
+class InvoiceInline(admin.StackedInline):
+    model = Invoice
+
+
+class DebtorAdmin(admin.ModelAdmin):
+    inlines = (InvoiceInline,)
+
+
+admin.site.register(Debtor, DebtorAdmin)
